eeb506f762a0dfee3172d4e5156957b5d0f107ec
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2023-02-03 09:13:58 +0800

    Add project files.

---
+++ SEO-Scrape-All-InONE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEO_Scrape_All_InONE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
